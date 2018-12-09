4a79db2eaa5a95aa4c2d893cbc96c5fc3911ac93
CRT <caleb_tulloss@brown.edu>
2018-12-09 00:12:27 -0500

    add lowpass->bandpass transformation

--- derivation.m
+++ derivation.m
@@ -4,7 +4,7 @@
 % Project: Channel Select Bandpass Filter for Transceiver
 
 clear;
-close;
+close all;
 
 %% Specs
 omega_center = 10e6 * 2*pi;
@@ -24,7 +24,6 @@ gain = 10^(gain_dB / 20);
 
 alpha_max = 0.5;   % dB
 alpha_min = 60;    % dB
-epsilon = sqrt(10^(alpha_max / 10) - 1);
 
 %% Transform to Lowpass for Design
 
@@ -34,15 +33,16 @@ omega_s2_norm = omega_s2 / omega_0;
 omega_p1_norm = omega_p1 / omega_0;
 omega_p2_norm = omega_p2 / omega_0;
 
+% transform to lowpass
 Omega_s1 = Q*(omega_s1_norm^2 - 1)/omega_s1_norm;
 Omega_s2 = Q*(omega_s2_norm^2 - 1)/omega_s2_norm;
-
 Omega_p1 = Q*(omega_p1_norm^2 - 1)/omega_p1_norm;
 Omega_p2 = Q*(omega_p2_norm^2 - 1)/omega_p2_norm;
 
 % Omega_s2_norm is a more strict requirement (sharper transition),
 % so use that
 
+epsilon = sqrt(10^(alpha_max / 10) - 1);
 atten_for_graph = alpha_min + 20*log(1/epsilon);
 % can look at the graph - looks like 5
 
@@ -52,14 +52,113 @@ atten_for_graph = alpha_min + 20*log(1/epsilon);
 
 %% Lowpass Transfer Function
 
-% look in design table - doesn't have exactly what we want...??
-
-% or use Matlab design - can we just use this?
+% or use Matlab design
 [ze,pe,ke] = ellip(n,0.5,60,Omega_p2,'s');
+% don't forget gain!
+ke = ke*gain;
+
+% plot lowpass just for fun
 [be,ae] = zp2tf(ze,pe,ke);
 [he,we] = freqs(be,ae,4096);
 figure
 plot(we,mag2db(abs(he)));
 title('Normalized Lowpass Response: 5^{th}-order Elliptic');
-ylabel('|H(j\omega)| (dB)');
+ylabel('|H(j\Omega)| (dB)');
 xlabel('Normalized \Omega (rad/s)');
+
+%% Transform to Bandpass
+
+% calculate w0 and Q for each of the cc pole pairs
+p1_w0 = abs(pe(1));
+p1_Q = abs(p1_w0 / (2*real(pe(1))));
+p2_w0 = abs(pe(3));
+p2_Q = abs(p2_w0 / (2*real(pe(3))));
+
+% follow the textbook derivation p.385-386 to calculate the
+% bandpass w0 and Q - 2 cc pairs for each of the lowpass cc pairs
+p1_a = p1_w0 / p1_Q;
+p1_b = p1_w0^2;
+p2_a = p2_w0 / p2_Q;
+p2_b = p2_w0^2;
+
+p1_q = sqrt(Q/p1_a * ((2*Q/p1_a + p1_b/(2*p1_a*Q)) + sqrt(...
+    (2*Q/p1_a + p1_b/(2*p1_a*Q))^2 - 1)));
+p2_q = sqrt(Q/p2_a * ((2*Q/p2_a + p2_b/(2*p2_a*Q)) + sqrt(...
+    (2*Q/p2_a + p2_b/(2*p2_a*Q))^2 - 1)));
+
+p1_w01 = p1_a*p1_q/(2*Q)+0.5*sqrt(p1_b/(Q^2) - 1/(p1_q^2));
+p1_w02 = 1/p1_w01;
+
+p2_w01 = p2_a*p2_q/(2*Q)+0.5*sqrt(p2_b/(Q^2) - 1/(p2_q^2));
+p2_w02 = 1/p2_w01;
+
+% for the lowpass real pole, the bandpass cc pole pair
+% has w0=1. calculate Q
+p3 = abs(pe(5));
+p3_q = Q/p3;
+
+% follow the textbook derivation p. 387-388 to calculate the bandpass zeros
+% signs are flipped in 2 and 4 because -a * -1/a = 1 as well as a/a=1!
+% ensures that zeros are cc pairs
+z1 = imag(ze(1));
+z1_w1 = sqrt(z1^2/(4*Q^2) + 1) - z1/(2*Q);
+z1_w2 = sqrt(z1^2/(4*Q^2) + 1) + z1/(2*Q);
+z2 = imag(ze(2));
+z2_w1 = -sqrt(z2^2/(4*Q^2) + 1) + z2/(2*Q);
+z2_w2 = -sqrt(z2^2/(4*Q^2) + 1) - z2/(2*Q);
+z3 = imag(ze(3));
+z3_w1 = sqrt(z3^2/(4*Q^2) + 1) - z3/(2*Q);
+z3_w2 = sqrt(z3^2/(4*Q^2) + 1) + z3/(2*Q);
+z4 = imag(ze(4));
+z4_w1 = -sqrt(z4^2/(4*Q^2) + 1) + z4/(2*Q);
+z4_w2 = -sqrt(z4^2/(4*Q^2) + 1) - z4/(2*Q);
+
+% check the transfer function - need to put in pole-zero form
+bandpass_z = [0 z1_w1 z1_w2 z2_w1 z2_w2 z3_w1 z3_w2 z4_w1 z4_w2]';
+bandpass_z = 1i*bandpass_z;
+
+% for the purpose of matlab filter plotting, which likes explicit cc poles
+% instead of w0,Q for a pole pair
+a = 1;
+b = p1_w01/p1_q;
+c = p1_w01^2;
+p = [a b c];
+bp12 = roots(p);
+
+a = 1;
+b = p1_w02/p1_q;
+c = p1_w02^2;
+p = [a b c];
+bp34 = roots(p);
+
+a = 1;
+b = p2_w01/p2_q;
+c = p2_w01^2;
+p = [a b c];
+bp56 = roots(p);
+
+a = 1;
+b = p2_w02/p2_q;
+c = p2_w02^2;
+p = [a b c];
+bp78 = roots(p);
+
+a = 1;
+b = 1/p3_q;
+c = 1;
+p = [a b c];
+bp910 = roots(p);
+
+bandpass_p = vertcat(bp12, bp34, bp56, bp78, bp910);
+
+% factor of 1/Q from the 1st order pole
+bandpass_k = ke / Q;
+
+% plot normalzied bandpass
+[bandpass_b,bandpass_a] = zp2tf(bandpass_z,bandpass_p,bandpass_k);
+[bandpass_h,bandpass_w] = freqs(bandpass_b,bandpass_a,10000);
+figure
+plot(bandpass_w,mag2db(abs(bandpass_h)));
+title('Normalized Bandpass Response: 10^{th}-order Elliptic');
+ylabel('|H(j\omega)| (dB)');
+xlabel('Normalized \omega (rad/s)');
